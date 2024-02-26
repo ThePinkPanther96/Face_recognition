a964b46e1fdae7f428c449801407ad5303e3c284
git <git@192.168.1.51>
2024-02-26 20:33:29 +0200

    .

--- Image_face_recognition.py
+++ Image_face_recognition.py
@@ -5,29 +5,26 @@ imagePath = 'kusay.jpg'
 
 #Reads the image as Color Image (By fefault as BGR not RGB)
 img = cv2.imread(imagePath)
+settings = ["haarcascade_frontalface_default.xml","haarcascade_profileface.xml","haarcascade_lefteye_2splits.xml"]
 
 # Convert to Gray Image for better computation efficiancy
 gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 # Load the Classifier
-face_classifier = cv2.CascadeClassifier(
-    cv2.data.haarcascades + "haarcascade_frontalface_default.xml"
-)
-
 # Face detection
-face = face_classifier.detectMultiScale(
-    gray_image, scaleFactor=1.1, minNeighbors=5, minSize=(40, 40)
-)
-
 # Drawing a Bounding Box
-for (x, y, w, h) in face:
-    cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 4)
+
+for i in settings:
+    postion = cv2.CascadeClassifier(cv2.data.haarcascades + i)
+    face = postion.detectMultiScale(gray_image, scaleFactor=1.1, minNeighbors=5, minSize=(40, 40))
+    for (x, y, w, h) in face:
+        cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
 # convert the image from the BGR format to RGB:
 img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
 
-plt.figure(figsize=(20,10))
+plt.figure(figsize=(10,10))
 plt.imshow(img_rgb)
 plt.axis('on')
 
